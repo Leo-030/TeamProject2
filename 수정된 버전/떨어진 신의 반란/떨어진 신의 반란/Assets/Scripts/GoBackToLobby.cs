1de74ddaaa311085f1b48a6ec355a4241c9f8256
Leo-030 <automatic06241039@gmail.com>
2023-05-07 14:06:26 +0900

    fix 05 07

--- 수정된 버전/떨어진 신의 반란/떨어진 신의 반란/Assets/Scripts/GoBackToLobby.cs
+++ 수정된 버전/떨어진 신의 반란/떨어진 신의 반란/Assets/Scripts/GoBackToLobby.cs
@@ -23,6 +23,27 @@ public class GoBackToLobby : MonoBehaviour
     {
         if (stageManager.GetComponent<StageManager>().stage > DataManager.instance.database.playerData.stageClear && stageManager.GetComponent<StageManager>().sub > 10)
         {
+            switch (stageManager.GetComponent<StageManager>().stage)
+            {
+                case 1:
+                    DataManager.instance.database.playerData.characterList.Add(1);
+                    break;
+                case 2:
+                    DataManager.instance.database.playerData.characterList.Add(2);
+                    break;
+                case 3:
+                    DataManager.instance.database.playerData.characterList.Add(3);
+                    break;
+                case 4:
+                    DataManager.instance.database.playerData.characterList.Add(4);
+                    break;
+                case 5:
+                    DataManager.instance.database.playerData.characterList.Add(5);
+                    break;
+                case 6:
+                    DataManager.instance.database.playerData.characterList.Add(6);
+                    break;
+            }
             DataManager.instance.database.playerData.stageClear = stageManager.GetComponent<StageManager>().stage;
             DataManager.instance.JsonSave();
         }
